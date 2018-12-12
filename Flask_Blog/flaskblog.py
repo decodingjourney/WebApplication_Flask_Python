c5556b3c2708b5ff69b595a68df6d86654bedc4b
anandkumarjhaic <37099338+anandkumarjhaic@users.noreply.github.com>
2018-12-12 13:01:11 +0530

    Web Application using flask and Python -- Exploring the concept of Templates

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Anand jha',
+        'title': 'Blog Post 1',
+        'content': 'First Blog Post',
+        'date_posted': 'December 12, 2018'
+    },
+    {
+        'author': 'Sonam jha',
+        'title': 'Blog Post 2',
+        'content': 'second Blog Post',
+        'date_posted': 'December 11, 2018'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
