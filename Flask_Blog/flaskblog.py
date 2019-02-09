1f091c9801bc6a894272898dbcbbdba49cdb3f81
anandkumarjhaic <37099338+anandkumarjhaic@users.noreply.github.com>
2019-02-09 19:34:49 +0530

    Login implemented and introduction of Database to the application

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('__name__')
-
 app.config['SECRET_KEY'] = '9c11906fbbcb91a791aa74f616aee41b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    confirm_password = db.Column(db.String(20), unique=False, nullable=False)
 
 posts = [
     {
@@ -38,9 +48,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@myblog.com' and form.password.data == 'password':
+            flash('You have been succesfully logged in! ', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Unsuccesful login, Please check the user credential', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
